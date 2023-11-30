375ad9ea925abfd4680019af7956ec389c32bb87
biku1998 <kumarsourabh228@gmail.com>
2023-11-30 10:10:27 +0530

    (feat) update maths

--- src/maths.ts
+++ src/maths.ts
@@ -1,21 +1,45 @@
 const countDigits = (no: number): number => {
   let temp = no;
   let noOfDigits = 0;
+
+  while (temp !== 0) {
+    temp = Math.floor(temp / 10);
+    noOfDigits++;
+  }
   return noOfDigits;
 };
 
 const isPalindrome = (no: number): boolean => {
-  return false;
+  let temp = no;
+  let reverse = 0;
+
+  while (temp !== 0) {
+    const lastDigit = Math.floor(temp % 10);
+    reverse = reverse * 10 + lastDigit;
+    temp = Math.floor(temp / 10);
+  }
+  return reverse === no;
 };
 
-const calculateFactorial = (no: number): number => {
+const calculateFactorial = (no: number) => {
   if (no === 0) return 1;
-  return no * calculateFactorial(no - 1);
+  let factorial = 1;
+
+  for (let i = 2; i <= no; i++) {
+    factorial = factorial * i;
+  }
+  return factorial;
+};
+
+const calculateFactorialRecursive = (no: number): number => {
+  if (no === 0) return 1;
+  return no * calculateFactorialRecursive(no - 1);
 };
 
 const main = () => {
-  //   console.log(`No of digits in ${434567} is ${countDigits(434567)}`);
-  //   console.log(`${767} is palindrome = ${isPalindrome(767)}`);
-  console.log(`Factorial of ${4} is ${calculateFactorial(4)}`);
+  console.log(`No of digits in ${1234567890} is ${countDigits(1234567890)}`);
+  console.log(`${121} is palindrome = ${isPalindrome(121)}`);
+  console.log(`Factorial of ${12} is ${calculateFactorial(12)}`);
+  console.log(`Factorial of ${12} is ${calculateFactorialRecursive(12)}`);
 };
 main();
